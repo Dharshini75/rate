8b65844052eb0ac43d0bfa00b0b5b639a1b451ba
dharshinianu751@gmail.com <dharshinianu751@gmail.com>
2025-07-01 20:31:00 +0530

    first

--- src/main/java/com/example/collection/CollectionApplication.java
+++ src/main/java/com/example/collection/CollectionApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CollectionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("a");
 		SpringApplication.run(CollectionApplication.class, args);
+		
 	}
 
 }
